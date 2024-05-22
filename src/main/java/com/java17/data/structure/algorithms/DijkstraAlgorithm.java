4f88123b24a459dcd2e514d64409146921e87486
DESKTOP-J8320O5\acode <stream.abhimanyu@gmail.com>
2024-05-22 20:08:22 +0530

    first commit

---
+++ src/main/java/com/java17/data/structure/algorithms/DijkstraAlgorithm.java
@@ -0,0 +1,13 @@
+package com.java17.data.structure.algorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DijkstraAlgorithm {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DijkstraAlgorithm.class, args);
+
+    }
+}
